ead86fd79a5f2ff9b60b116532ba1bc954d87887
markus.hein <hein-markus@web.de>
2023-01-31 21:55:38 +0100

    hide keys

--- lib/config/base_config.dart
+++ lib/config/base_config.dart
@@ -1,6 +1,4 @@
 const appointmentGraphqlUrl =
     "https://nameless-brook-400132.eu-central-1.aws.cloud.dgraph.io/graphql";
-const appointmentApiKey =
-    "Mjg4N2I1N2QyODgyMTViMDcyZTYyNzQ1OGVmMDA2ZGE="; //String.fromEnvironment('APPOINTMENT_API_KEY');
-
-const googleMapsApiKey = "AIzaSyCw9xqljt8jPkwnv_aAVjXmhI4k8G2cTa0";
+const appointmentApiKey =String.fromEnvironment('APPOINTMENT_API_KEY');
+const googleMapsApiKey = String.fromEnvironment('GOOGLE_MAPS_API_KEY');
